d7de71de15d90c7ec1ff7327107676b0a1dea38f
Edwin Lunandy <elunandy@newsignature.com>
2015-08-31 14:35:08 -0400

    Full width page template styling.

--- wp-content/themes/navigator/page_full-width.php
+++ wp-content/themes/navigator/page_full-width.php
@@ -10,12 +10,11 @@
 get_header(); ?>
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-8">
+			<div class="col-xs-12">
 				<div id="primary" class="content-area">
 					<main id="main" class="site-main" role="main">
 
 						<?php while ( have_posts() ) : the_post(); ?>
-
 							<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 							<?php
@@ -30,7 +29,6 @@ get_header(); ?>
 					</main><!-- #main -->
 				</div><!-- #primary -->
 			</div>
-			<?php get_sidebar(); ?>
 		</div>
 	</div>
 <?php get_footer(); ?>
