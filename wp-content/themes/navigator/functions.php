7ffb07753960caaecf6b781b526c74528b71efc0
Edwin Lunandy <elunandy@newsignature.com>
2015-08-31 14:03:39 -0400

    Add Typekit.

--- wp-content/themes/navigator/functions.php
+++ wp-content/themes/navigator/functions.php
@@ -131,6 +131,8 @@ function navigator_scripts() {
 
 	wp_enqueue_script( 'navigator-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+//	wp_enqueue_script( 'typekit', get_template_directory_uri() . '/assets/js/typekit.js', array(), '', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
