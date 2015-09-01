73f5b7e512a20b7f10d0a9591dbf01c3c5365e79
Edwin Lunandy <elunandy@newsignature.com>
2015-09-01 14:18:52 -0400

    Test REST API from different domain.

--- wp-content/themes/navigator/app/app.module.js
+++ wp-content/themes/navigator/app/app.module.js
@@ -1,6 +1,10 @@
 angular.module('navigatorApp', ['ngRoute'])
-    .config(function ($routeProvider, $locationProvider) {
+    .config(function ($routeProvider, $locationProvider, $httpProvider) {
         $locationProvider.html5Mode(true);
+
+        $httpProvider.defaults.useXDomain = true;
+
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
 
         $routeProvider
             .when('/healthy-community-navigator/', {
@@ -8,6 +12,19 @@ angular.module('navigatorApp', ['ngRoute'])
                 controller: 'mainController'
             })
     })
-    .controller('mainController', function () {
+    .controller('mainController', function ($scope, $http, $routeParams) {
+        $http.get('wp-json/posts/').success(function (res) {
+            $scope.posts = res;
+        });
+
+        $http.get(' http://themes.population-2.com/dev/peekaboo-wp/wp-json/posts').success(function (res2) {
+            $scope.posts2 = res2;
+        });
+
+
         console.log('Main file loaded.');
     })
+
+
+
+
